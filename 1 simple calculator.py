1080f29ac9654b92c39b4e976a812720510b4189
maheshpabbisetti <113224451+maheshpabbisetti@users.noreply.github.com>
2023-02-08 10:56:58 +0530

    output
    25
    100
    add:  125
    sub:  -75
    mul:  2500
    div:  0.25
    cal:  None

---
+++ 1 simple calculator.py
@@ -0,0 +1,12 @@
+m=int(input())
+n=int(input())
+def cal():
+    s=m+n
+    sub=m-n
+    mul=m*n
+    div=m/n
+    print("add: ",s)
+    print("sub: ",sub)
+    print("mul: ",mul)
+    print("div: ",div)
+print("cal: ",cal())
