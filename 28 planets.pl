25636a16c61f0f7cda90fe3252faa90cdd4e3bb1
maheshpabbisetti <113224451+maheshpabbisetti@users.noreply.github.com>
2023-02-12 12:26:40 +0530

    Create 28 planets.pl

---
+++ 28 planets.pl
@@ -0,0 +1,38 @@
+% Define the planets and their characteristics
+planet(mercury).
+planet(venus).
+planet(earth).
+planet(mars).
+planet(jupiter).
+planet(saturn).
+planet(uranus).
+planet(neptune).
+
+% Define the planet's diameter in kilometers
+diameter(mercury, 4879).
+diameter(venus, 12104).
+diameter(earth, 12756).
+diameter(mars, 6792).
+diameter(jupiter, 139000).
+diameter(saturn, 116000).
+diameter(uranus, 50724).
+diameter(neptune, 49244).
+
+% Define the planet's distance from the sun in million kilometers
+distance(mercury, 57.91).
+distance(venus, 108.2).
+distance(earth, 149.6).
+distance(mars, 227.9).
+distance(jupiter, 778.5).
+distance(saturn, 1433.5).
+distance(uranus, 2872.5).
+distance(neptune, 4495.1).
+
+% Define the planet's number of moons
+moons(mercury, 0).
+moons(venus, 0).
+moons(earth, 1).
+moons(mars, 2).
+moons(jupiter, 79).
+moons(saturn, 82).
+moons(uranus, 27).
